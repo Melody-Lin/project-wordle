576b852d0d1a505bd40182ae5326f6c81e79ea10
Melody-Lin <yiihsuanlin@gmail.com>
2025-01-26 16:39:08 -0800

    fifth exercise

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function GuessInput({ handleSubmitGuess }) {
+function GuessInput({ handleSubmitGuess, gameStatus }) {
   const [tentativeGuess, setTentativeGuess] = React.useState('');
 
   function handleSubmit(event) {
@@ -23,6 +23,7 @@ function GuessInput({ handleSubmitGuess }) {
         maxLength={5}
         pattern='[a-zA-Z]{5}'
         titile='5 letter word'
+        disabled={gameStatus !== 'running'}
         onChange={(event) => {
           const nextGuess = event.target.value.toUpperCase();
           setTentativeGuess(nextGuess);
